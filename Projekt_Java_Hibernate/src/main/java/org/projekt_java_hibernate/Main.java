7122e1dfcfc4f373d9dbba98d546b14705a67c6a
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-17 21:26:36 +0100

    initial commit

---
+++ Projekt_Java_Hibernate/src/main/java/org/projekt_java_hibernate/Main.java
@@ -0,0 +1,10 @@
+package org.projekt_java_hibernate;
+
+/**
+ * Created by Adam Seweryn
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
