31c381c42cdd7e167476e4cdc710a6f15d967125
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-01-18 20:16:48 +0100

    Plik konfiguracyjny i dopisanie zaleznosci

--- Projekt_Java_Hibernate/src/main/java/org/projekt_java_hibernate/App.java
+++ Projekt_Java_Hibernate/src/main/java/org/projekt_java_hibernate/App.java
@@ -3,7 +3,7 @@ package org.projekt_java_hibernate;
 /**
  * Created by Adam Seweryn
  */
-public class Main {
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
